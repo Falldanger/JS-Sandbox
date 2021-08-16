242ac90c1bfef7fca3c089652bf52d6a5d347194
Falldanger <bohdan.murga@yandex.ua>
2021-08-17 00:27:18 +0300

    Use bind, call and apply methods. Using prototype with this

--- Context/app.js
+++ Context/app.js
@@ -7,5 +7,44 @@ function hello() {
 const person = {
     name: 'Jack',
     age: 25,
-    sayHello: hello
+    sayHello: hello,
+    sayHelloWindow: hello.bind(document),
+    logInfo: function (job, phone) {
+        console.group(`${this.name} info:`)
+        console.log(`Name is ${this.name}`)
+        console.log(`Age is ${this.age}`)
+        console.log(`Job is ${job}`)
+        console.log(`Phone is ${job}`)
+        console.groupEnd()
+    }
 }
+
+const admin = {
+    name: 'admin',
+    age: 23
+}
+
+// const fnAdminInfoLog = person.logInfo.bind(admin)
+// fnAdminInfoLog('Backend dev','380506069066')
+
+// person.logInfo.bind(admin,'Backend dev','380506069066')()
+// person.logInfo.call(admin,'Backend dev','380506069066')
+person.logInfo.apply(admin, ['Backend dev', '380506069066'])
+
+//
+
+const array = [1, 2, 3, 4, 5, 6]
+
+function multBy(arr, n) {
+    return arr.map(function (i) {
+        return i * n
+    })
+}
+
+Array.prototype.multBy = function (n){
+return this.map(function (i){
+    return i*n
+})
+}
+
+console.log(array.multBy(10))
